4147fe2b28ba4765869dadeb5b3038cf872a594f
LP <LP@Rabah-Lem>
2023-05-13 22:10:10 -0400

    initial commit

--- src/main/java/com/mycompany/bonjournom/BonjourNom.java
+++ src/main/java/com/mycompany/bonjournom/BonjourNom.java
@@ -12,6 +12,6 @@ package com.mycompany.bonjournom;
 public class BonjourNom {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello rabah!");
     }
 }
