87608a5afbd26f7f75fcc77de3e5870c9f194656
LP <LP@Rabah-Lem>
2023-05-24 22:49:28 -0400

    hola

--- src/main/java/com/mycompany/bonjournom/BonjourNom.java
+++ src/main/java/com/mycompany/bonjournom/BonjourNom.java
@@ -12,6 +12,7 @@ package com.mycompany.bonjournom;
 public class BonjourNom {
 
     public static void main(String[] args) {
-        System.out.println("Hello marhaba!");
+        System.out.println("Hello world marhaba hola!");
     }
+    
 }
